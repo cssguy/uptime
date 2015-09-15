b351ca599cee381b1d5eac9e2cee74441114b24c
chereshnya3000 <chereshnya3000@yandex.ru>
2015-09-15 22:32:59 +0300

    New_uptime

--- uptime.js
+++ uptime.js
@@ -1,9 +1,3 @@
-var data = [
-  {value: 91.56},
-  {value: 91.52},
-  {value: 91.51}
-];
-
 // $(".uptime_values").css("color", "grey"); 
 // $("обращение к селектору: class или id").функция(работа над элементами, например изменение стилей, удаление эл-ов, добавление эл-ов)(значение);
 // документация по jQuery - http://api.jquery.com/
@@ -18,32 +12,53 @@ $(".uptime_block").append("<div class='uptime_values'></div>")
 
 /* Генерируем случайное значение от 90 до 100 */
 
-var minNumber = 9000;
-var maxNumber = 10000;
-var porog_1 = 94;
-var porog_2 = 96;
+var minNumber = 90;
+var maxNumber = 100;
 
 var magic = 7;
 var sum = 0;
 
+var defineColor = function(number, elm)
+{
+  var porog_1 = 94;
+  var porog_2 = 96;
+  var porog_3 = 99;
+
+    if (number < porog_1) {
+    elm.addClass("less_then_94");
+  } else if (number < porog_2 && number > porog_1) {
+    elm.addClass("less_then_96");
+  } else {
+    elm.addClass("less_then_100");
+  }
+
+}
+
+  var spanContainer = $(".uptime_values");
+
 for (var i = 0; i < magic; i++) 
 {
-  var randomNumber = (Math.floor(Math.random()*(maxNumber-minNumber+1)+minNumber))/100;
+  var randomNumber = (Math.random()*(maxNumber-minNumber+1)+minNumber);
   /* Вставляем значения случайных чисел в span элементы */
-  $(".uptime_values").append("<span>" + randomNumber.toFixed(2) + "</span>"); 
+  spanContainer.append("<span>" + randomNumber.toFixed(2) + "</span>"); 
+
+  var span = spanContainer.find("span").last();
+
+  defineColor(randomNumber, span);
 
   sum += randomNumber;
 }
 
+var medium_value_elem = $(".medium_value");
+
 /* Выводим среднее значение полученных данных от 90 до 100 */
 var medium_number = sum/magic;
-$(".medium_value").append(medium_number.toFixed(2));
+medium_value_elem.append(medium_number.toFixed(2));
 
-/* Меняем цвет значений в зависимости от результата */
-if (medium_number < porog_1) {
-  $(".medium_value").addClass("less_then_94");
-} else if (medium_number < porog_2 && medium_number > porog_1) {
-  $(".medium_value").addClass("less_then_96");
-} else {
-  $(".medium_value").addClass("less_then_100");
-}
+defineColor(medium_number, medium_value_elem);
+
+
+
+
+
+
